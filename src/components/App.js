5ac89919b4a1b800d779c9278b3d340449bc2b61
remarkablejames <lascasasjames85@gmail.com>
2022-04-20 22:49:19 +0200

    Making a request to the Youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import Searchbar from "./SearchBar";
+import Youtube from "../api/Youtube";
 
 class App extends React.Component {
+  onTermSubmit = async (searchTerm) => {
+    console.log(searchTerm);
+    const response = await Youtube.get("/search", {
+      params: {
+        q: searchTerm,
+      },
+    });
+    console.log(response);
+  };
   render() {
     return (
       <div className="ui container">
-        <Searchbar />
+        <Searchbar formSubmit={this.onTermSubmit} />
       </div>
     );
   }
