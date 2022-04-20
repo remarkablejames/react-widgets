4905fdef0893c09377a4e2e00899a6254bb3aeb1
remarkablejames <lascasasjames85@gmail.com>
2022-04-20 22:32:12 +0200

    Creating the Searchbar component of the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
-function App() {
-  return (
-    <div>
-      <h1>This is App component</h1>
-    </div>
-  );
+import React from "react";
+import Searchbar from "./SearchBar";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="ui container">
+        <Searchbar />
+      </div>
+    );
+  }
 }
 
 export default App;
