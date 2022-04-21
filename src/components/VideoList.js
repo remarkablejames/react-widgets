20e8a831774ac63d10d503e3c897f7e976a92ccc
remarkablejames <lascasasjames85@gmail.com>
2022-04-21 08:44:20 +0200

    rendered list of fetched videos on the screen

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,4 +1,15 @@
-function VideoList(props) {
-  return <div>{props.videos.length}</div>;
+import VideoItem from "./VideoItem";
+
+function VideoList({ videos }) {
+  const renderedList = videos.map((video) => {
+    return <VideoItem key={video.id.videoId} video={video} />;
+  });
+
+  return (
+    <div>
+      {videos.length}
+      <div>{renderedList}</div>
+    </div>
+  );
 }
 export default VideoList;
